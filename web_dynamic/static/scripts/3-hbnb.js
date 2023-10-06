a6d2e1a57b3c1e88d9e48b116e6f49562b6a9499
MercyNJ <mercinjuguna@gmail.com>
2023-10-06 10:13:56 +0000

    filter spaces by amenity

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -2,66 +2,41 @@
 
 $(document).ready(function () {
   const selectedAmenities = {};
-
-  function updateAmenitiesTag () {
-    const selectedAmenitiesList = Object.values(selectedAmenities);
-    $('.amenities h4').text(selectedAmenitiesList.join(', '));
-  }
-
-  $('.amenity-checkbox').change(function () {
-    const amenityId = $(this).data('id');
-    const amenityName = $(this).data('name');
-
+  $(document).on('change', "input[type='checkbox']", function () {
     if (this.checked) {
-      selectedAmenities[amenityId] = amenityName;
+      selectedAmenities[$(this).data('id')] = $(this).data('name');
     } else {
-      delete selectedAmenities[amenityId];
+      delete selectedAmenities[$(this).data('id')];
     }
-
-    updateAmenitiesTag();
-  });
-  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
-    if (data.status === 'OK') {
-      $('div#api_status').addClass('available');
+    const lst = Object.values(selectedAmenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(selectedAmenities).join(', '));
     } else {
-      $('div#api_status').removeClass('available');
+      $('div.amenities > h4').html('&nbsp;');
     }
   });
 });
 
-const HOST = 'http://0.0.0.0:5001';
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+  if (textStatus === 'success') {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  }
+});
 
-$.post({
-  url: `${HOST}/api/v1/places_search`,
-  data: JSON.stringify({}),
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  success: (data) => {
-    data.forEach((place) =>
-      $('section.places').append(
-        `<article>
-          <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">$${place.price_by_night}</div>
-          </div>
-          <div class="information">
-            <div class="max_guest">${place.max_guest} Guest${
-              place.max_guest !== 1 ? 's' : ''
-            }</div>
-            <div class="number_rooms">${place.number_rooms} Bedroom${
-              place.number_rooms !== 1 ? 's' : ''
-            }</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-              place.number_bathrooms !== 1 ? 's' : ''
-            }</div>
-          </div> 
-          <div class="description">
-            ${place.description}
-          </div>
-        </article>`
-      )
-    );
-  },
-  dataType: 'json'
+$.ajax({
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search',
+  data: '{}',
+  dataType: 'json',
+  contentType: 'application/json',
+  success: function (data) {
+    for (let i = 0; i < data.length; i++) {
+      const place = data[i];
+      $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+    }
+  }
 });
